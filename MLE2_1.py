31b74cc1d3480dd2c6704bfad7db9e08e67590ef
robert501128 <robert501128@gmail.com>
2016-10-23 20:33:29 -0700

    change the plot function

--- MLE2_1.py
+++ MLE2_1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import random 
 s=900
-data=10000
+data=10
 def lik(p):
     w = p[:-1].reshape((4,1))
     sigma = p[-1]
@@ -21,12 +21,23 @@ np.random.seed(s)
 t= np.random.normal(0.1+2*x+x**2+3*x**3,1)
 lik_model = minimize(lik, np.array([1.0,1.0,1.0,1.0,1.0]), method='Powell')
 w = lik_model['x'][:-1].reshape((4,1))
-_x=np.array([x**0,x,x**2,x**3])
-y=sum(w*_x) 
-plt.scatter(x,t)
+fig= plt.figure() 
+ax = fig.add_subplot(111)
+ax.scatter(x,t,label='t')
 x = np.array([random.uniform(-100,100) for i in range(10000)])
 x.sort()
 _x=np.array([x**0,x,x**2,x**3])
 y=sum(w*_x)
-plt.plot(x,y)
+ax.plot(x,y,label='y')
+ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+strGraph=""
+for i in range(len(w)):
+	strGraph+="w"+str(i)+": %.10f\n" % w[i][0]
+strGraph+='beta: %.10f' % lik_model['x'][-1]
+ax.text(0.95, 0.01, strGraph,
+        verticalalignment='bottom', horizontalalignment='right',
+        transform=ax.transAxes,
+        color='blue', fontsize=15)
+
 plt.show()
--- MLE2_2.py
+++ MLE2_2.py
@@ -22,14 +22,25 @@ t= np.random.normal(0.1+2*x+x**2+3*x**3,1)
 lik_model = minimize(lik, np.array([1.0,1.0,1.0,1.0,1.0,1.0,1.0])\
 	, method='Powell')
 w = lik_model['x'][:-1].reshape((6,1))
-print w
 
-plt.scatter(x,t)
-x = np.array([random.uniform(-100,100) for i in range(100)])
+fig= plt.figure() 
+ax = fig.add_subplot(111)
+ax.scatter(x,t,label='t')
+x = np.array([random.uniform(-100,100) for i in range(10000)])
 x.sort()
 _x=np.array([x**0,x,x**2,x**3,x**4,x**5])
 y=sum(w*_x)
+ax.plot(x,y,label='y')
+ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+strGraph=""
+for i in range(len(w)):
+	strGraph+="w"+str(i)+": %.10f\n" % w[i][0]
+strGraph+='beta: %.10f' % lik_model['x'][-1]
+ax.text(0.95, 0.01, strGraph,
+        verticalalignment='bottom', horizontalalignment='right',
+        transform=ax.transAxes,
+        color='blue', fontsize=15)
 
-plt.plot(x,y)
 plt.show()
 
